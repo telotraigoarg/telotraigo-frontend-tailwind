4917c575d637cba95fbb609c23a887e160e9b7ab
telotraigoarg <service.digitales.2024@gmail.com>
2025-07-14 16:27:51 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Image from "next/image";
+import Logo from "../public/logo-te-lo-traigo-autos.png";
 
 export default function Home() {
   const [pais, setPais] = useState("Dubái");
